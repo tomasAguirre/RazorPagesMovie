084c6e9875048a337720a54001bb11736f67b5e6
DEVELOPERPC\jonat <jonathan3aguirre@outllook.com>
2024-12-27 09:39:21 -0600

    Add project files.

---
+++ RazorPagesMovie/Controllers/FilmController.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorPagesMovie.Models;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Controllers
+{
+    public class FilmController : Controller
+    {
+        public static List<Film> filmsList;
+        public FilmController()
+        {
+            if (filmsList is null) 
+            {
+                Film f1 = new Film(1, "Batman", DateTime.Now);
+                Film f2 = new Film(2, "Padrino", DateTime.Now);
+                Film f3 = new Film(3, "Super man", DateTime.Now);
+                filmsList = new List<Film>
+                { f1,f2,f3 };
+            }
+        }
+
+        public async Task<IActionResult> Index() 
+            =>await Task.Run(() => View(filmsList));
+
+        //public async Task<IActionResult> Index() =>
+        //{
+        //    return await Task.Run(() => View(filmsList));
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id) 
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Film film = filmsList.First(x => x.Id == id);
+
+            if (film == null) {
+                return NotFound();
+            }
+
+            return View("Edit", film);
+            ;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditPost([FromForm] Film film) 
+        {
+            if (film.Id == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                var filmToUpdate = filmsList.FirstOrDefault(x => x.Id == film.Id);
+
+                if (filmToUpdate != null)
+                {
+                    filmToUpdate.Name = film.Name;
+                    filmToUpdate.commingOut = film.commingOut;
+                }
+            }
+            catch 
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                          "Try again, and if the problem persists " +
+                          "see your system administrator.");
+            }
+
+
+            return View("Index", filmsList);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Film film = filmsList.First(x => x.Id == id);
+
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return View("Delete", film);
+            ;
+        }
+
+        [HttpPost]
+        public IActionResult DeleteFilm([FromForm] Film film) 
+        {
+            if (film.Id == null) 
+            {
+                return NotFound();
+            }
+            try
+            {
+                var filmToDelete = filmsList.FirstOrDefault(f => f.Id == film.Id);
+                filmsList.Remove(filmToDelete);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                          "Try again, and if the problem persists " +
+                          "see your system administrator.");
+            }
+            return View("Index", filmsList);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateFilm([FromForm] Film film) 
+        {
+            try
+            {
+                filmsList.Add(film);
+            }
+            catch (Exception) 
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+          "Try again, and if the problem persists " +
+          "see your system administrator.");
+            }
+            return View("Index", filmsList);
+        }
+    }
+}
---
+++ RazorPagesMovie/Models/Film.cs
@@ -0,0 +1,22 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RazorPagesMovie.Models
+{
+    public class Film
+    {
+        public Film()
+        {
+        }
+
+        public Film(int id, string name, DateTime commingOut)
+        {
+            Id = id;
+            Name = name;
+            this.commingOut = commingOut;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime commingOut { get; set; }
+    }
+}
